7236980e64d2b086e8eac4fbb37e920785f560dc
fanolans <mailenolan@gmail.com>
2023-09-01 08:20:38 +0700

    Refactor model poster null safety

--- lib/domain/movie_model.dart
+++ lib/domain/movie_model.dart
@@ -4,69 +4,31 @@ class MoviesModel {
   final String title;
   final String year;
   final String runtime;
-  final String poster;
+  final String? poster;
+
   MoviesModel({
     required this.title,
     required this.year,
     required this.runtime,
-    required this.poster,
+    this.poster,
   });
 
-  MoviesModel copyWith({
-    String? title,
-    String? year,
-    String? runtime,
-    String? poster,
-  }) {
-    return MoviesModel(
-      title: title ?? this.title,
-      year: year ?? this.year,
-      runtime: runtime ?? this.runtime,
-      poster: poster ?? this.poster,
-    );
-  }
+  factory MoviesModel.fromRawJson(String str) =>
+      MoviesModel.fromJson(json.decode(str));
 
-  Map<String, dynamic> toMap() {
-    return {
-      'title': title,
-      'year': year,
-      'runtime': runtime,
-      'poster': poster,
-    };
-  }
+  String toRawJson() => json.encode(toJson());
 
-  factory MoviesModel.fromMap(Map<String, dynamic> map) {
-    return MoviesModel(
-      title: map['title'] ?? '',
-      year: map['year'] ?? '',
-      runtime: map['runtime'] ?? '',
-      poster: map['poster'] ?? '',
-    );
-  }
+  factory MoviesModel.fromJson(Map<String, dynamic> json) => MoviesModel(
+        title: json["Title"],
+        year: json["Year"],
+        runtime: json["Runtime"],
+        poster: json["Poster"],
+      );
 
-  String toJson() => json.encode(toMap());
-
-  factory MoviesModel.fromJson(String source) =>
-      MoviesModel.fromMap(json.decode(source));
-
-  @override
-  String toString() {
-    return 'MoviesModel(title: $title, year: $year, runtime: $runtime, poster: $poster)';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is MoviesModel &&
-        other.title == title &&
-        other.year == year &&
-        other.runtime == runtime &&
-        other.poster == poster;
-  }
-
-  @override
-  int get hashCode {
-    return title.hashCode ^ year.hashCode ^ runtime.hashCode ^ poster.hashCode;
-  }
+  Map<String, dynamic> toJson() => {
+        "Title": title,
+        "Year": year,
+        "Runtime": runtime,
+        "Poster": poster,
+      };
 }
