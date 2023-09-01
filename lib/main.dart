61141b4e9fbd257a8572c46666847811c41cbf0d
fanolans <mailenolan@gmail.com>
2023-09-01 09:49:05 +0700

    Refactor folder name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_test_primaya/presentation/home_page.dart';
+import 'package:flutter_test_primaya/presentation/pages/home_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
