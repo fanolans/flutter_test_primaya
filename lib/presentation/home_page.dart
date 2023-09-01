342408453518c17e907d77823189a6c1cb86b601
fanolans <mailenolan@gmail.com>
2023-09-01 09:42:45 +0700

    Extract to list widget

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,75 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_test_primaya/application/movie_provider.dart';
+import 'package:flutter_test_primaya/presentation/movie_list_widget.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, ref) {
-    final movieData = ref.watch(moviesDataProvider);
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Movies List"),
+        title: const Text('Movie List'),
       ),
-      body: movieData.when(
-        loading: () => const Center(
-          child: CircularProgressIndicator(),
-        ),
-        data: (movieData) {
-          return ListView.builder(
-            itemCount: movieData.length,
-            itemBuilder: (_, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                child: Card(
-                  color: Theme.of(context).primaryColor,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Title: ${movieData[index].title}',
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        'Year: ${movieData[index].year}',
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        'Runtime: ${movieData[index].runtime}',
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Poster: ',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          Container(
-                            height: MediaQuery.of(context).size.height / 3,
-                            width: MediaQuery.of(context).size.width / 2,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                  movieData[index].poster.toString(),
-                                ),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
-        error: (error, s) => Text(error.toString()),
-      ),
+      body: const ListWidget(),
     );
   }
 }
---
+++ lib/presentation/movie_list_widget.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test_primaya/application/movie_provider.dart';
+
+class ListWidget extends ConsumerWidget {
+  const ListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final movieData = ref.watch(moviesDataProvider);
+    return movieData.when(
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+      data: (movieData) {
+        return ListView.builder(
+          itemCount: movieData.length,
+          itemBuilder: (_, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+              child: Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Title: ${movieData[index].title}',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        'Year: ${movieData[index].year}',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        'Runtime: ${movieData[index].runtime}',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Poster: ',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Container(
+                            height: MediaQuery.of(context).size.height / 3,
+                            width: MediaQuery.of(context).size.width / 2,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  movieData[index].poster.toString(),
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+      error: (error, s) => Text(error.toString()),
+    );
+  }
+}
